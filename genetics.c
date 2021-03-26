7344b9d07a7e501b492fb95a80a7499457352796
Miguel Cóias <miguelgcoias@gmail.com>
2021-03-26 22:46:06 +0000

    fixed Knuth-Morris-Pratt

--- genetics.c
+++ genetics.c
@@ -1,52 +1,53 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 /** String matching algorithms **/
-/* Naive algorithm */
 void naive(char *text, char *pattern, size_t text_sz, size_t pattern_sz);
-/* Knuth-Morris-Pratt algorithm */
-void kmp(char *text, char *pattern, size_t *lps, size_t text_sz, size_t pattern_sz);
-/* Boyer-Moore algorithm */
+void kmp(char *text, char *pattern, size_t text_sz, size_t pattern_sz);
 void bm();
 
 /** Helper functions **/
-/* Read from stdin */
 char *read();
-/* Construct prefix/suffix table for Knuth-Morris-Pratt algorithm*/
-void constructlps(size_t *lps, char *pattern, size_t pattern_sz);
+void kmpTable(size_t *table, char *pattern, size_t pattern_sz);
 
 int main() {
-  char *text = NULL, *pattern;
+  char *text = NULL, *pattern = NULL;
+  size_t text_sz = 0, pattern_sz = 0;
   int command = getc(stdin);
-  size_t text_sz, pattern_sz;
 
   while (command != 'X') {
     switch (command) {
       case 'T': {
-        /* Free memory used for previous text */
         if (text) {
           free(text);
           text = NULL;
         }
-        text_sz = 0;
         text = read(&text_sz);
         break;
       }
-      /* Naive case */
       case 'N': {
+        if (pattern) {
+          free(pattern);
+          pattern = NULL;
+        }
         pattern = read(&pattern_sz);
+        assert(pattern && text);
+        assert(pattern_sz > 0 && text_sz > 0);
         naive(text, pattern, text_sz, pattern_sz);
         break;
       }
-      /* Knuth-Morris-Pratt case */
       case 'K': {
+        if (pattern) {
+          free(pattern);
+          pattern = NULL;
+        }
         pattern = read(&pattern_sz);
-        size_t lps[pattern_sz];
-        constructlps(lps, pattern, pattern_sz);
-        kmp(text, pattern, lps, text_sz, pattern_sz);
+        assert(pattern && text);
+        assert(pattern_sz > 0 && text_sz > 0);
+        kmp(text, pattern, text_sz, pattern_sz);
         break;
       }
-      /* Boyer-Moore case */
       case 'B': {
         break;
       }
@@ -65,33 +66,28 @@ char *read(size_t *str_sz) {
   /* Skip trailing whitespace */
   getc(stdin);
 
-  *str_sz = 0;
-
   char *str = NULL, *tmp = NULL;
   size_t buffer_sz = 0;
   int ch = getc(stdin);
+  *str_sz = 0;
   
   while (ch != '\n') {
     /* Memory allocated BUFSIZ bytes at a time */
     if (buffer_sz <= *str_sz) {
       buffer_sz += BUFSIZ;
       tmp = realloc(str, buffer_sz);
-
       /* To do: deal with eventual situations where this happens */
       if (!tmp) {
         free(str);
         str = NULL;
         break;
       }
-
       str = tmp;
     }
-
     str[*str_sz] = ch;
     *str_sz = *str_sz + 1;
     ch = getc(stdin);
   }
-
   return str;
 }
 
@@ -106,50 +102,46 @@ void naive(char *text, char *pattern, size_t text_sz, size_t pattern_sz) {
     if (j == pattern_sz)
       printf("%zu ", i);
   }
-
   printf("\n");
 }
 
-void kmp(char *text, char *pattern, size_t *lps, size_t text_sz, size_t pattern_sz) {
-  size_t i = 0, j = 0, n_comp = 0;
+void kmp(char *text, char *pattern, size_t text_sz, size_t pattern_sz) {
+  size_t i, k = 0;
+  size_t table[pattern_sz];
 
-  while (i < text_sz) {
-    if (j == pattern_sz) {
-      printf("%zu ", i - j);
-      j = lps[j-1];
-      n_comp++;
-    }
+  kmpTable(table, pattern, pattern_sz);
 
-    if (text[i] == pattern[j]) {
-      i++;
-      j++;
+  for (i = 0; i < text_sz; i++) {
+    /* printf("i: %zu\n", i);
+    printf("k: %zu\n", k); */
+    while (k > 0 && pattern[k] != text[i])
+      k = table[k-1];
+    if (pattern[k] == text[i])
+      k++;
+    if (k == pattern_sz) {
+      /* printf("k: %zu \n", k); */
+      k = table[k-1];
+      printf("%zu ", i - pattern_sz + 1);
+      /* printf("i: %zu \n", i); */
     }
-    else {
-      if (j > 0)
-        j--;
-      else
-        i++;
-    }
-
-    n_comp++;
   }
-
-  if (j == pattern_sz)
-    printf("%zu ", i - j);
-  
-  printf("\n%zu\n", n_comp);
+  /* printf("Pattern size: %zu\n", pattern_sz); */
+  printf("\n");
 }
 
-void constructlps(size_t *lps, char *pattern, size_t pattern_sz) {
-  lps[0] = 0;
+void kmpTable(size_t *table, char *pattern, size_t pattern_sz) {
+  size_t i, k = 0;
 
-  if (pattern_sz > 1) {
-    size_t i;
-    for (i = 1; i < pattern_sz; i++) {
-      if (pattern[i] == pattern[lps[i-1]])
-        lps[i] = lps[i-1] + 1;
-      else
-        lps[i] = 0;
-    }
+  table[0] = 0;
+  /* printf("%zu ", table[0]); */
+
+  for (i = 1; i < pattern_sz; i++) {
+    while (k > 0 && pattern[k] != pattern[i])
+      k = table[k-1];
+    if (pattern[k] == pattern[i])
+      k++;
+    table[i] = k;
+    /* printf("%zu ", table[i]); */
   }
+  /* printf("\n"); */
 }
