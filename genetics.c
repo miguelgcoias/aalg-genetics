60489f9f40ac382930bcd7951c0d1240e330e82f
Miguel Cóias <miguelgcoias@gmail.com>
2021-04-02 02:14:54 +0100

    broken Boyer-Moore...

--- genetics.c
+++ genetics.c
@@ -5,10 +5,10 @@
 /** String matching algorithms **/
 void naive(char *text, char *pattern, size_t text_sz, size_t pattern_sz);
 void kmp(char *text, char *pattern, size_t text_sz, size_t pattern_sz);
-void bm();
+void bm(char *text, char *pattern, size_t text_sz, size_t pattern_sz);
 
 /** Helper functions **/
-char *read();
+char *read(size_t *str_sz);
 void kmpTable(size_t *table, char *pattern, size_t pattern_sz);
 
 int main() {
@@ -17,6 +17,10 @@ int main() {
   int command = getc(stdin);
 
   while (command != 'X') {
+    if (pattern) {
+      free(pattern);
+      pattern = NULL;
+    }
     switch (command) {
       case 'T': {
         if (text) {
@@ -27,28 +31,21 @@ int main() {
         break;
       }
       case 'N': {
-        if (pattern) {
-          free(pattern);
-          pattern = NULL;
-        }
         pattern = read(&pattern_sz);
-        assert(pattern && text);
-        assert(pattern_sz > 0 && text_sz > 0);
+        assert(pattern && text && pattern_sz && text_sz);
         naive(text, pattern, text_sz, pattern_sz);
         break;
       }
       case 'K': {
-        if (pattern) {
-          free(pattern);
-          pattern = NULL;
-        }
         pattern = read(&pattern_sz);
-        assert(pattern && text);
-        assert(pattern_sz > 0 && text_sz > 0);
+        assert(pattern && text && pattern_sz && text_sz);
         kmp(text, pattern, text_sz, pattern_sz);
         break;
       }
       case 'B': {
+        pattern = read(&pattern_sz);
+        assert(pattern && text && pattern_sz && text_sz);
+        bm(text, pattern, text_sz, pattern_sz);
         break;
       }
       default:
@@ -59,7 +56,7 @@ int main() {
     command = getc(stdin);
   }
 
-  return EXIT_SUCCESS;
+  return 0;
 }
 
 char *read(size_t *str_sz) {
@@ -96,7 +93,7 @@ void naive(char *text, char *pattern, size_t text_sz, size_t pattern_sz) {
 
   for (i = 0; i < text_sz - pattern_sz; i++) {
     for (j = 0; j < pattern_sz; j++) {
-      if (text[i+j] != pattern[j])
+      if (text[i + j] != pattern[j])
         break;
     }
     if (j == pattern_sz)
@@ -115,12 +112,12 @@ void kmp(char *text, char *pattern, size_t text_sz, size_t pattern_sz) {
     /* printf("i: %zu\n", i);
     printf("k: %zu\n", k); */
     while (k > 0 && pattern[k] != text[i])
-      k = table[k-1];
+      k = table[k - 1];
     if (pattern[k] == text[i])
       k++;
     if (k == pattern_sz) {
       /* printf("k: %zu \n", k); */
-      k = table[k-1];
+      k = table[k - 1];
       printf("%zu ", i - pattern_sz + 1);
       /* printf("i: %zu \n", i); */
     }
@@ -129,6 +126,7 @@ void kmp(char *text, char *pattern, size_t text_sz, size_t pattern_sz) {
   printf("\n");
 }
 
+/* Do this inside kmp() */
 void kmpTable(size_t *table, char *pattern, size_t pattern_sz) {
   size_t i, k = 0;
 
@@ -137,11 +135,176 @@ void kmpTable(size_t *table, char *pattern, size_t pattern_sz) {
 
   for (i = 1; i < pattern_sz; i++) {
     while (k > 0 && pattern[k] != pattern[i])
-      k = table[k-1];
+      k = table[k - 1];
     if (pattern[k] == pattern[i])
       k++;
     table[i] = k;
     /* printf("%zu ", table[i]); */
   }
   /* printf("\n"); */
+}
+
+void bm(char *text, char *pattern, size_t text_sz, size_t pattern_sz) {
+  size_t i;
+  size_t nc = 0;
+
+  /*** Bad character rule implementation ***/
+
+  /* badc holds the right-most position of each character that appears in the
+  pattern; 'A' is on position 0, 'C' on 1, 'G' on 2 and 'T' on 3
+  If a character does not appear in the pattern, then its value is the maximum
+  size_t possible, i.e. (size_t) -1 */
+  /* size_t bc[4] = {-1, -1, -1, -1}; */
+  
+  /* To do: explore better ways to do this */
+  /* for (i = 0; i < pattern_sz; i++) {
+    switch (pattern[i]) {
+      case ('A'):
+        bc[0] = (i > bc[0] || bc[0] == -1) ? i : bc[0];
+        break;
+      case ('C'):
+        bc[1] = (i > bc[1] || bc[1] == -1) ? i : bc[1];
+        break;
+      case ('G'):
+        bc[2] = (i > bc[2] || bc[2] == -1) ? i : bc[2];
+        break;
+      case ('T'):
+        bc[3] = (i > bc[3] || bc[3] == -1) ? i : bc[3];
+        break;
+    }
+  } */
+
+  /* bc holds the right-most position of every character, or (size_t) -1 if it
+  does not appear in the pattern */
+  size_t bc[256];
+  size_t ch;
+  for (i = 0; i < 256; i++) {
+    bc[i] = -1;
+  }
+  for (i = 0; i < pattern_sz; i++) {
+    ch = (size_t) pattern[i];
+    bc[ch] = (i > bc[ch] || bc[ch] == -1) ? i : bc[ch];
+  }
+
+  /* printf("bc values: %zu %zu %zu %zu\n", bc[0], bc[1], bc[2], bc[3]); */
+
+  /*** Strong good suffix rule implementation ***/
+  /** Z algorithm **/
+  size_t z[pattern_sz]; /* Z values */
+  size_t left = 0; /* Left box endpoint */
+  size_t right = 0; /* Right box endpoint */
+  size_t j; /* Auxiliary variable for loops */
+  size_t gs[pattern_sz];
+  char pattern_r[pattern_sz]; /* Reversed pattern */
+
+  /* Reverse pattern */
+  for (j = 0; j < pattern_sz; j++)
+    pattern_r[pattern_sz - j - 1] = pattern[j];
+
+  z[0] = pattern_sz;
+  for (i = 1; i < pattern_sz; i++) {
+    if (i > right) {
+      j = 0;
+      while (i + j < pattern_sz && pattern_r[j] == pattern_r[j + i])
+        j++;
+      z[i] = j;
+      if (j > 0) {
+        left = i;
+        right = i + j - 1;
+      }
+    }
+    else {
+      if (z[i - left] < right - i + 1)
+        z[i] = z[i - left];
+      else {
+        j = right + 1;
+        while (j < pattern_sz && pattern_r[j] == pattern_r[j - i])
+          j++;
+        z[i] = j - i;
+        left = i;
+        right = j - 1;
+      }
+    }
+  }
+
+  /* Reverse z in place */
+  size_t temp;
+  for (j = 0; j < pattern_sz/2; j++) {
+    temp = z[pattern_sz - j - 1];
+    z[pattern_sz - j - 1] = z[j];
+    z[j] = temp;
+  }
+  
+  for (j = 0; j < pattern_sz; j++)
+    gs[j] = 0;
+  for (j = 0; j < pattern_sz - 1; j++) {
+    gs[z[j]] = pattern_sz - j - 2;
+  }
+
+  printf("gs before max_gs: ");
+  for (j = 0; j < pattern_sz; j++) {
+    printf("%zu ", gs[j]);
+  }
+  printf("\n");
+
+  size_t max_gs = 0;
+  for (j = 1; j < pattern_sz - 1; j++) {
+    max_gs = (z[j] == j + 1) ? pattern_sz - j - 2 : max_gs;
+    gs[j] = (gs[j] == 0) ? max_gs : gs[j];
+  }
+
+  /* Missing bad character integration */
+  size_t bc_skip;
+  for (i = 0; i < text_sz - pattern_sz + 1; i++) {
+    j = 0;
+    while (j < pattern_sz && text[i + pattern_sz - j - 1] == pattern[pattern_sz - j - 1]) {
+      j++;
+      nc++;
+    }
+    if (j != pattern_sz) {
+      nc++;
+      ch = (size_t) text[i + pattern_sz - j - 1];
+      if (bc[ch] == -1)
+        bc_skip = pattern_sz - j - 1;
+      else {
+        if (pattern_sz - j - 1 > bc[ch])
+          bc_skip = pattern_sz - j - bc[ch] - 2;
+        else
+          bc_skip = 0;
+      }
+      i += (gs[j] > bc_skip) ? gs[j] : bc_skip;
+    }
+    else {
+      printf("%zu ", i);
+      i += gs[pattern_sz - 1];
+    }
+  }
+  printf("\n%zu \n", nc);
+
+
+  printf("z: ");
+  for (j = 0; j < pattern_sz; j++) {
+    printf("%zu ", z[j]);
+  }
+  printf("\n");
+
+  /* printf("           ");
+  for (j = 0; j < ((pattern_sz < 10) ? pattern_sz : 10); j++) {
+    printf("%zu  ", j);
+  }
+  for (j = 10; j < pattern_sz; j++) {
+    printf("%zu ", j);
+  }
+  printf("\n"); */
+/*   printf("String: ");
+  for (j = 0; j < pattern_sz; j++) {
+    printf("%c", pattern[j]);
+  } */
+  /* printf("\n"); */
+  /* printf("gs: ");
+  for (j = 0; j < pattern_sz; j++) {
+    printf("%zu ", gs[j]);
+  }
+  printf("\n"); */
+
 }
